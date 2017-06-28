5f9a37d5700c268b5da0be64a4e0a17b59a012fe
Nicky Kwan <nickykwn@gmail.com>
2017-06-28 15:00:14 -0400

    first blogger commit

--- blogger/db/schema.rb
+++ blogger/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170627194235) do
+ActiveRecord::Schema.define(version: 20170628184400) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
